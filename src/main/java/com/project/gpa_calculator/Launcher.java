5b749abf000a6ae5552c928f21995386e1cc0a84
Rafsan Riasat <riasat1011@gmail.com>
2025-11-09 17:33:28 +0600

    project creation

---
+++ src/main/java/com/project/gpa_calculator/Launcher.java
@@ -0,0 +1,9 @@
+package com.project.gpa_calculator;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
